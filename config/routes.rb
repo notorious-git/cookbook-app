20fae40901d527c43203575f3e110cd276c1f6fd
Peter Jang <peter.x.jang@gmail.com>
2018-10-15 21:21:55 -0500

    Add RESTful recipe create action

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     get "/recipes" => "recipes#index"
+    post "/recipes" => "recipes#create"
     get "/recipes/:id" => "recipes#show"
   end
 end
