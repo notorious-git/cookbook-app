629511cc102a57ad17112560523e47fadbf58b5a
Peter Jang <peter.x.jang@gmail.com>
2018-10-16 21:30:12 -0500

    Add model methods to format some attributes in the view

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,30 @@
 class Recipe < ApplicationRecord
+  def formatted_ingredients
+    ingredients_list = []
+    ingredients.split(", ").each do |ingredient|
+      ingredients_list << ingredient.downcase.tr(".", "")
+    end
+    return ingredients_list
+  end
+
+  def formatted_directions
+    directions_list = []
+    directions.split(", ").each do |direction|
+      directions_list << direction.downcase.tr(".", "")
+    end
+    return directions_list
+  end
+
+  def friendly_created_at
+    created_at.strftime("%B %e, %Y")
+  end
+
+  def friendly_prep_time
+    hours = prep_time / 60
+    minutes = prep_time % 60
+    result = ""
+    result += "#{hours} hour(s) " if hours > 0
+    result += "#{minutes} minute(s)" if minutes > 0
+    return result
+  end
 end
--- app/views/api/recipes/show.json.jbuilder
+++ app/views/api/recipes/show.json.jbuilder
@@ -2,5 +2,9 @@ json.id @recipe.id
 json.title @recipe.title
 json.chef @recipe.chef
 json.prep_time @recipe.prep_time
+json.friendly_prep_time @recipe.friendly_prep_time
 json.ingredients @recipe.ingredients
 json.directions @recipe.directions
+json.formatted_ingredients @recipe.formatted_ingredients
+json.formatted_directions @recipe.formatted_directions
+json.created_at @recipe.friendly_created_at
