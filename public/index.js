e3d50b8a1903e3fb6241d162e5f1d375e4a19cdd
Peter Jang <peter.x.jang@gmail.com>
2018-11-12 18:30:01 -0600

    Use JavaScript to clone a recipe card template

---
+++ public/index.js
@@ -0,0 +1,7 @@
+var recipeTemplate = document.querySelector("#recipe-card");
+var recipeContainer = document.querySelector(".row");
+
+recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
+recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
+recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
+recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
