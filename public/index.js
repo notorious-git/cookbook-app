00d5bbdc5c3f46b702a435c8901107477cc18395
Peter Jang <peter.x.jang@gmail.com>
2018-11-12 19:17:14 -0600

    Use axios to get recipe data

--- public/index.js
+++ public/index.js
@@ -1,7 +1,19 @@
-var recipeTemplate = document.querySelector("#recipe-card");
-var recipeContainer = document.querySelector(".row");
+/* global axios */
 
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
-recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
+// RUBY WEB REQUEST
+// response = HTTP.get("/api/recipes")
+// recipes = response.parse
+
+// JAVASCRIPT WEB REQUEST
+axios.get("/api/recipes").then(function(response) {
+  var recipes = response.data;
+  console.log("the recipes are", recipes);
+
+  var recipeTemplate = document.querySelector("#recipe-card");
+  var recipeContainer = document.querySelector(".row");
+
+  recipes.forEach(function(recipe) {
+    recipeContainer.appendChild(recipeTemplate.content.cloneNode(true));
+  });
+});
+
